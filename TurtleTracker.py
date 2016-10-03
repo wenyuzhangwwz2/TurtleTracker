59bf1642672560ee2a45e33cd18bd7a391eed20b
wenyuzhangwwz2 <wenyu.zhang@duke.edu>
2016-10-03 12:29:19 -0400

    Adds while loop to read all lines

--- TurtleTracker.py
+++ TurtleTracker.py
@@ -14,21 +14,27 @@ fileObj = open(fileName, 'r')
 # Read the first line from the open file object
 lineString = fileObj.readline()
 
+# Use a while loop to read each line, one at a time, until the end of the file is reached
+while lineString:
+
+    # Use the split command to parse the items in lineString into a list object
+    lineData = lineString.split("\t")
+
+    # Assign variables to specfic items in the list
+    recordID = lineData[0]              # ARGOS tracking record ID
+    obsDateTime = lineData[2]           # Observation date and time (combined)
+    obsDate = obsDateTime.split()[0]    # Observation date - first item in obsDateTime list object
+    obsTime = obsDateTime.split()[1]    # Observation time - second item in obsDateTime list object
+    obsLC = lineData[3]                 # Observation Location Class
+    obsLat = lineData[5]                # Observation Latitude
+    obsLon = lineData[6]                # Observation Longitude
+
+    # Print information to the user
+    print "According to record " + recordID, 
+    print "Sara was seen at " + obsLat + " d LAT; " + obsLat + " d LON on " + obsDate
+
+    # Read in the next line
+    lineString = fileObj.readline()
+    
 # Close the file object
 fileObj.close()
-
-# Use the split command to parse the items in lineString into a list object
-lineData = lineString.split("\t")
-
-# Assign variables to specfic items in the list
-recordID = lineData[0]              # ARGOS tracking record ID
-obsDateTime = lineData[2]           # Observation date and time (combined)
-obsDate = obsDateTime.split()[0]    # Observation date - first item in obsDateTime list object
-obsTime = obsDateTime.split()[1]    # Observation time - second item in obsDateTime list object
-obsLC = lineData[3]                 # Observation Location Class
-obsLat = lineData[5]                # Observation Latitude
-obsLon = lineData[6]                # Observation Longitude
-
-# Print information to the user
-print "According to record " + recordID, 
-print "Sara was seen at " + obsLat + " d LAT; " + obsLat + " d LON on " + obsDate
